2144f66d67407f5ac6c61c3e90684de754e43239
Conor Keating <conor.keating@intercom.io>
2023-05-16 15:38:47 +0100

    rename index to server

--- api/server.js
+++ api/server.js
@@ -4,23 +4,8 @@ const app = express(),
       bodyParser = require("body-parser");
       port = 3080;
 
-// place holder for the data
-const users = [];
-
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../my-app/out')));
-
-app.get('/api/users', (req, res) => {
-  console.log('api/users called!')
-  res.json(users);
-});
-
-app.post('/api/user', (req, res) => {
-  const user = req.body.user;
-  console.log('Adding user:::::', user);
-  users.push(user);
-  res.json("user addedd");
-});
 
 app.get('/', (req,res) => {
     res.statusCode = 200
--- api/webpack.config.js
+++ api/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './index.js',
+  entry: './server.js',
   mode: 'production',
   target: 'node',
   output: {
